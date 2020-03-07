eaddb62951f4cbbb440cd9317aa2c9c640cd342b
Adam Netočný <adam@netocny.sk>
2020-03-07 17:18:15 +0100

    Upgrade to @angular/core@9.0.5

--- src/app/abstract.view.component.ts
+++ src/app/abstract.view.component.ts
@@ -1,10 +1,11 @@
-import {EventEmitter, Input, Output} from '@angular/core';
+import {Directive, EventEmitter, Input, Output} from '@angular/core';
 import {Meta} from '@angular/platform-browser';
 import {AbstractComponent} from './abstract.component';
 import {ViewHeader} from './detail/detail';
 import {Route} from './routing/route';
 import {RoutingService} from './routing/routing.service';
 
+@Directive()
 export abstract class AbstractViewComponent extends AbstractComponent {
 
     @Output()
